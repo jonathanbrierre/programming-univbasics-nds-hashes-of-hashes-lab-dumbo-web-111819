f99fb13731b32fc87421bf3b042ed424ac135583
jonathan brierre <jonathanbrierre@yahoo.com>
2019-10-16 23:17:52 +0000

    Automatically backed up by Learn

--- lib/hoh.rb
+++ lib/hoh.rb
@@ -61,7 +61,7 @@ def naming_system
      sub_category: {
        label: "Class",
        sub_category: {
-          label: "Order"
+          label: "Order",
           sub_category: {
             label: "Family",
             sub_category: {
